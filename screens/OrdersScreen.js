5c08b4ad4d36b19a212a24ab8418998ab8784084
Remsko <hello.localidad@gmail.com>
2025-08-28 19:25:00 +0200

    chore: filtres uniques (OrdersInlineFilters) et imports nettoyés

--- screens/OrdersScreen.js
+++ screens/OrdersScreen.js
@@ -1,4 +1,3 @@
-import InlineFilters from '../components/InlineFilters';
 import React, { useEffect, useMemo, useRef, useState, useCallback } from 'react';
 import { ScrollView, View, Text, StyleSheet, Platform, Switch, TouchableOpacity } from 'react-native';
 import OrderCard from '../components/OrderCard';
